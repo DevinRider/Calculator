8e81a76c632e812871312c71b7db0a08d25b3a64
Devin Rider <devinrrider@gmail.com>
2014-06-03 07:40:12 -0700

    Initial Commit

---
+++ Calculator/DEVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DEVAppDelegate.h
+//  Calculator
+//
+//  Created by Devin on 6/3/14.
+//  Copyright (c) 2014 Devin Rider. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DEVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Devin on 6/3/14.
+//  Copyright (c) 2014 Devin Rider. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DEVAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DEVAppDelegate class]));
+    }
+}
